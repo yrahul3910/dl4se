4f0c057b4a2f8f552950dbca3508a42027712d95
Rahul Yedida <yrahul3910@gmail.com>
2020-12-30 23:28:38 -0500

    Added GHOST v2 results

---
+++ defect/GHOST/interp.py
@@ -0,0 +1,13 @@
+from raise_utils.interpret import DODGEInterpreter
+import os
+import numpy as np
+
+files = os.listdir('./ghost-v2-log')
+for file in files:
+    print(file, end=': ')
+    interp = DODGEInterpreter(
+        files=[f'./ghost-v2-log/{file}'], metrics=['f1', 'pd', 'prec'])
+    res = interp.interpret()
+    print(round(np.median(res[file]['f1']), 3) * 100, end='\t')
+    print(round(np.median(res[file]['pd']), 3) * 100, end='\t')
+    print(round(np.median(res[file]['prec']), 3) * 100)
